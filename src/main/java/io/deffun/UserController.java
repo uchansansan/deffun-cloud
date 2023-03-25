ccfc930c77c8b91cb34e098e0807001c3b495e48
uchansansan <uchansansan@gmail.com>
2023-03-25 14:12:05 +0700

    upload file REST complited

--- src/main/java/io/deffun/UserController.java
+++ src/main/java/io/deffun/UserController.java
@@ -2,22 +2,35 @@ package io.deffun;
 
 import io.deffun.usermgmt.UserData;
 import io.deffun.usermgmt.UserService;
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Post;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.micronaut.http.multipart.CompletedFileUpload;
 import io.micronaut.http.multipart.StreamingFileUpload;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
 import jakarta.inject.Inject;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static io.micronaut.http.HttpStatus.CONFLICT;
+import static io.micronaut.http.MediaType.MULTIPART_FORM_DATA;
+import static io.micronaut.http.MediaType.TEXT_PLAIN;
 
 @Controller("/api/users")
-@Secured(SecurityRule.IS_AUTHENTICATED)
+@Secured(SecurityRule.IS_ANONYMOUS)
 public class UserController {
     @Inject
     private UserService userService;
@@ -28,20 +41,28 @@ public class UserController {
         return userService.getByEmail(email);
     }
 
-    @Secured(SecurityRule.IS_ANONYMOUS)
-    @Post("/upload_ssh_key")
-    public HttpResponse<Void> uploadSshKey( StreamingFileUpload file) throws IOException {
-        FileWriter f = new FileWriter("1.pub");
-        f.write(file.toString());
-        f.close();
-//Authentication authentication,  String sshPublicKey,
-        //String email = (String) authentication.getAttributes().get("email");
-//        if (email == null) {
-//            // use AuthenticationExceptionHandler // or create your own
-//            throw new AuthenticationException("no email");
-//        }
-        //userService.uploadSshKey(email, sshPublicKey);
-        return HttpResponse.ok();
+
+    @ExecuteOn(TaskExecutors.IO)
+    @Post(value = "/upload_file", consumes = MULTIPART_FORM_DATA, produces = TEXT_PLAIN)
+    public Publisher<HttpResponse<String>> upload(StreamingFileUpload file) {
+        System.out.println(file.getFilename());
+        File tempFile;
+        try {
+            tempFile = File.createTempFile(file.getFilename(), "temp");
+        } catch (IOException e) {
+            return Mono.error(e);
+        }
+        Publisher<Boolean> uploadPublisher = file.transferTo(tempFile);
+
+        return Mono.from(uploadPublisher)
+                .map(success -> {
+                    if (success) {
+                        return HttpResponse.ok("Uploaded");
+                    } else {
+                        return HttpResponse.<String>status(CONFLICT)
+                                .body("Upload Failed");
+                    }
+                });
     }
 
     @Get("/ssh_key")
